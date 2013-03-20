0d86a78619dfb281f374314dacee47cbbaebac09
Bonnie <wholenotes@yahoo.com>
2013-03-19 17:28:25 -0700

    setting things up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giraffe::Application.config.secret_token = 'eeeeaa5ae394db2dbcbdd53ef432f9a260f6aa55b0100ea5c85f01757ee6d861e73e80fc18022be059f437a3a62f7bf098b9dacbb9cd3b4f63e1e8c5aa5d8f81'
